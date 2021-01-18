4b8af6dca0c71e156434a0ab16d586e1658fbb04
Sepichat <4227791+Sepichat@users.noreply.github.com>
2021-01-18 14:52:39 +0100

    Adds possibility to give money to a different player

--- scripts/actorOverride.js
+++ scripts/actorOverride.js
@@ -2,6 +2,11 @@ import { PlayerDialog } from "./dialog.js";
 
 function activateListeners(html) {
   this.giveItemModuleActivateListeners(html);
+  addGiveItemButton.bind(this)(html);
+  addGiveCurrency.bind(this)(html);
+}
+
+function addGiveItemButton(html) {
   $(`
     <a class="item-control item-give" title="Give item">
       <i class="fas fa-hands-helping"></i>
@@ -14,14 +19,17 @@ function activateListeners(html) {
   });
 }
 
-export const overrideActorSheet = () => {
-  Object.values(CONFIG.Actor.sheetClasses).forEach((type) => Object.values(type).forEach((sheet) => {
-    if (sheet.id.includes('dnd5e.ActorSheet5eCharacter')) {
-      sheet.cls.prototype.giveItemModuleActivateListeners = sheet.cls.prototype.activateListeners;
-      sheet.cls.prototype.activateListeners = activateListeners;
-    }
-  }));
-};
+function addGiveCurrency(html) {
+  $(`
+    <a class="currency-control currency-give" title="Give currency">
+      <i class="fas fa-hands-helping"></i>
+    </a>
+  `).insertAfter(html.find(".currency-convert.rollable"));
+  html.find(".currency-control.currency-give").on("click", (e) => {
+    e.preventDefault();
+    giveCurrency.bind(this)();
+  });
+}
 
 function getItemFromInvoByName(actor, name) {
   return actor.items.find(t => t.data.name === name);
@@ -32,37 +40,81 @@ function giveItem(currentItemId) {
   const listPC = game.actors.entities.filter(a => a.hasPlayerOwner);
   const filteredPCList = listPC.filter(a => a.id !== this.actor.id);
   const d = new PlayerDialog(({playerId, quantity}) => {
-      const actor = game.actors.get(playerId);
-      const currentItem = currentActor.items.find(item => item.id === currentItemId);
-      const currentItemQuantity = currentItem.data.data.quantity;
-      if (quantity > currentItemQuantity) {
-        return ui.notifications.error(`You cannot offer more items than you posses`);
-      } else {
-        const updateItem = {
-          "data.quantity": currentItem.data.data.quantity - quantity
-        }
-        currentItem.update(updateItem).then(res => {
-            const duplicatedItem = duplicate(currentItem);
-            duplicatedItem.data.quantity = quantity;
-            const existingItem = getItemFromInvoByName(actor, duplicatedItem.name);
-            if (existingItem) {
-              const updateItem = {
-                "data.quantity": existingItem.data.data.quantity + quantity
-              }
-              existingItem.update(updateItem);
-            } else {
-              actor.createEmbeddedEntity("OwnedItem", duplicatedItem);
-            }
-            console.log(`Giving item: ${currentItem.id} to actor ${actor.id}`);
-            if (currentItem.data.data.quantity === 0) {
-                currentItem.delete();
-            }
-          }
-        );
+    const actor = game.actors.get(playerId);
+    const currentItem = currentActor.items.find(item => item.id === currentItemId);
+    const currentItemQuantity = currentItem.data.data.quantity;
+    if (quantity > currentItemQuantity) {
+      return ui.notifications.error(`You cannot offer more items than you posses`);
+    } else {
+      const updateItem = {
+        "data.quantity": currentItem.data.data.quantity - quantity
       }
-    },
+      currentItem.update(updateItem).then(res => {
+        const duplicatedItem = duplicate(currentItem);
+        duplicatedItem.data.quantity = quantity;
+        const existingItem = getItemFromInvoByName(actor, duplicatedItem.name);
+        if (existingItem) {
+          const updateItem = {
+            "data.quantity": existingItem.data.data.quantity + quantity
+          }
+          existingItem.update(updateItem);
+        } else {
+          actor.createEmbeddedEntity("OwnedItem", duplicatedItem);
+        }
+        console.log(`Giving item: ${currentItem.id} to actor ${actor.id}`);
+        if (currentItem.data.data.quantity === 0) {
+          currentItem.delete();
+        }
+      }
+      );
+    }
+  },
     {acceptLabel: "Offer Item", filteredPCList}
   );
   d.render(true);
 }
 
+function giveCurrency() {
+  const currentActor = this.actor;
+  const listPC = game.actors.entities.filter(a => a.hasPlayerOwner);
+  const filteredPCList = listPC.filter(a => a.id !== this.actor.id);
+  const d = new PlayerDialog(({playerId, pp, gp, ep, sp, cp}) => {
+    const actor = game.actors.get(playerId);
+    const currentCurrency = currentActor.data.data.currency;
+    const currentTargetCurrency = actor.data.data.currency;
+    if (pp > currentCurrency.pp || gp > currentCurrency.gp || ep > currentCurrency.ep || sp > currentCurrency.sp || cp > currentCurrency.cp) {
+      return ui.notifications.error(`You cannot offer more currency than you posses`);
+    } else {
+      const updateGold = {
+        "data.currency.pp": currentCurrency.pp - pp,
+        "data.currency.gp": currentCurrency.gp - gp,
+        "data.currency.ep": currentCurrency.ep - ep,
+        "data.currency.sp": currentCurrency.sp - sp,
+        "data.currency.cp": currentCurrency.cp - cp,
+      };
+      currentActor.update(updateGold).then(res => {
+        const updateTargetGold = {
+          "data.currency.pp": currentTargetCurrency.pp + pp,
+          "data.currency.gp": currentTargetCurrency.gp + gp,
+          "data.currency.ep": currentTargetCurrency.ep + ep,
+          "data.currency.sp": currentTargetCurrency.sp + sp,
+          "data.currency.cp": currentTargetCurrency.cp + cp,
+        };
+        console.log(`Giving currency: pp:${pp}, gp:${gp}, ep:${ep}, sp:${sp}, cp:${cp}, to actor ${actor.id}`);
+        actor.update(updateTargetGold);
+      });
+    }
+  },
+    {acceptLabel: "Offer Currency", filteredPCList, currency: true}
+  );
+  d.render(true);
+}
+
+export const overrideActorSheet = () => {
+  Object.values(CONFIG.Actor.sheetClasses).forEach((type) => Object.values(type).forEach((sheet) => {
+    if (sheet.id.includes('dnd5e.ActorSheet5eCharacter')) {
+      sheet.cls.prototype.giveItemModuleActivateListeners = sheet.cls.prototype.activateListeners;
+      sheet.cls.prototype.activateListeners = activateListeners;
+    }
+  }));
+};
--- scripts/dialog.js
+++ scripts/dialog.js
@@ -3,14 +3,23 @@ export class PlayerDialog extends Dialog {
       if (typeof (options) !== "object") {
         options = {};
       }
-      let applyChanges = false;
-      super({
-        title: "Offer item to someone",
-        content: `
-        <form>
-          <div class="form-group">
-            <label>Quantity:</label>
-            <input type=number min="1" id="quantity" name="quantity" value="1">
+      const giveItemTemplate = `
+      <form>
+        <div class="form-group">
+          <label>Quantity:</label>
+          <input type=number min="1" id="quantity" name="quantity" value="1">
+          <label>Players:</label>
+          <select name="type" id="player">
+            ${options.filteredPCList.reduce((acc, currentActor) => {
+              return acc + `<option value="${currentActor.id}">${currentActor.name}</option>`
+            }, '')}
+          </select>
+        </div>
+      </form>`;
+      const giveCurrencyTemplate = `
+      <form>
+        <div class="form-group">
+          <div class="give-item-dialog player">
             <label>Players:</label>
             <select name="type" id="player">
               ${options.filteredPCList.reduce((acc, currentActor) => {
@@ -18,7 +27,32 @@ export class PlayerDialog extends Dialog {
               }, '')}
             </select>
           </div>
-        </form>`,
+          <div class="give-item-dialog currency">
+            <label>Platinium:</label>
+            <input type=number id="pp" name="pp" value="">
+          </div>
+          <div class="give-item-dialog currency">
+            <label>Gold:</label>
+            <input type=number id="gp" name="gp" value="">
+          </div>
+          <div class="give-item-dialog currency">
+            <label>Electrum:</label>
+            <input type=number id="ep" name="ep" value="">
+          </div>
+          <div class="give-item-dialog currency">
+            <label>Silver:</label>
+            <input type=number id="sp" name="sp" value="">
+          </div>
+          <div class="give-item-dialog currency">
+            <label>Copper:</label>
+            <input type=number id="cp" name="cp" value="">
+          </div>
+        </div>
+      </form>`;
+      let applyChanges = false;
+      super({
+        title: !options.currency ? "Offer item to someone" : "Offer currency to someone",
+        content: options.currency ? giveCurrencyTemplate : giveItemTemplate,
         buttons: {
           yes: {
             icon: "<i class='fas fa-check'></i>",
@@ -33,14 +67,33 @@ export class PlayerDialog extends Dialog {
         default: "yes",
         close: () => {
           if (applyChanges) {
-            const playerId = document.getElementById('player').value;
-            let quantity = document.getElementById('quantity').value;
-            if (isNaN(quantity)) {
-              console.log("Item quantity invalid");
-              return ui.notifications.error(`Item quantity invalid.`);
+            if (options.currency) {
+              const playerId = document.getElementById('player').value;
+              let pp = document.getElementById('pp').value;
+              let gp = document.getElementById('gp').value;
+              let ep = document.getElementById('ep').value;
+              let sp = document.getElementById('sp').value;
+              let cp = document.getElementById('cp').value;
+              if (isNaN(pp) || isNaN(gp) || isNaN(ep) || isNaN(sp) || isNaN(cp)) {
+                console.log("Currency quantity invalid");
+                return ui.notifications.error(`Currency quantity invalid.`);
+              }
+              pp = Number(pp);
+              gp = Number(gp);
+              ep = Number(ep);
+              sp = Number(sp);
+              cp = Number(cp);
+              callback({playerId, pp, gp, ep, sp, cp});
+            } else {
+              const playerId = document.getElementById('player').value;
+              let quantity = document.getElementById('quantity').value;
+              if (isNaN(quantity)) {
+                console.log("Item quantity invalid");
+                return ui.notifications.error(`Item quantity invalid.`);
+              }
+              quantity = Number(quantity);
+              callback({playerId, quantity});
             }
-            quantity = Number(quantity);
-            callback({playerId, quantity});
           }
         }
       });
